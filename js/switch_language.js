3ae0b6f37f8c6614750acda571fef89e4ef7af1e
ClariflaroCode <julietawatts.ar@gmail.com>
2025-08-09 18:53:14 -0300

    Multi lenguaje en los placeholders

--- js/switch_language.js
+++ js/switch_language.js
@@ -7,6 +7,7 @@ function iniciarCambioIdioma() {
     let idiomasDisponibles = document.querySelectorAll(".idioma");
     let idiomaDescripcion = document.getElementById("idioma-elegido-text");
     const texts = document.querySelectorAll('[data-i18n]');
+    const placeholders = document.querySelectorAll('[data-i18n-placeholder]');
 
     let traducciones = {};
     const idiomaDelNavegador = navigator.language.split("-")[0].toUpperCase(); //el navegador le manda "es-113213810132" a los idiomas
@@ -63,32 +64,33 @@ function iniciarCambioIdioma() {
             console.log("español");
             idiomaEscogido.src = "../img/bandera-argentina.png";
             idiomaDescripcion.innerHTML = "ES";
-
-            for (const text of texts) {
-                const clave = text.dataset.i18n;
-                if(traducciones[idiomaSeleccionado] && traducciones[idiomaSeleccionado][clave]) {
-                    text.textContent = traducciones[idiomaSeleccionado][clave];
-                } 
-                else {
-                    console.warn(`No se encontró traducción para clave '${clave}' en idioma '${idiomaSeleccionado}'`);
-                }
-
-            }
-
         } else { //por defecto está en el idioma universal (?)
             console.log("inglés");
             idiomaEscogido.src = "../img/bandera-inglesa.png";
-            idiomaDescripcion.innerHTML = "EN";
-            for (const text of texts) {
-                const clave = text.dataset.i18n;
-                if(traducciones[idiomaSeleccionado] && traducciones[idiomaSeleccionado][clave]) {
-                    text.textContent = traducciones[idiomaSeleccionado][clave];
-                } 
-                else {
-                    console.warn(`No se encontró traducción para clave '${clave}' en idioma '${idiomaSeleccionado}'`);
-                }
+            idiomaDescripcion.innerHTML = "EN";    
+        }
+
+        for (const text of texts) {
+            const clave = text.dataset.i18n;
+            if(traducciones[idiomaSeleccionado] && traducciones[idiomaSeleccionado][clave]) {
+                text.textContent = traducciones[idiomaSeleccionado][clave];
+            } 
+            else {
+                console.warn(`No se encontró traducción para clave '${clave}' en idioma '${idiomaSeleccionado}'`);
+            }
+
+        }
+        //placeholders:
+        for(const input of placeholders) {
+            const clave = input.dataset.i18nPlaceholder; //dato curioso: los guiones que se usen luego de data- se vuelven camelCase en JavaScript
+            if(traducciones[idiomaSeleccionado] && traducciones[idiomaSeleccionado][clave]) {
+                input.placeholder = traducciones[idiomaSeleccionado][clave];
+            } 
+            else {
+                console.warn(`No se encontró traducción para clave '${clave}' en idioma '${idiomaSeleccionado}'`);
             }
         }
+
     }
     
 }
